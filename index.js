4313d2ec16ebd2a6ea6f18cc6100edad31c54cbf
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-05-16 20:44:58 -0300

    Conectar a MongoDB

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 // Crear el servidor
 const app = express();
 
+// Conectar a la base de datos
+conectarDB();
 
-// puerto y arrancar el servidor
-app.listen(4000, () => {
-    console.log('Servidor funcionando')
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`)
 })
+
+// // puerto y arrancar el servidor
+// app.listen(4000, () => {
+//     console.log('Servidor funcionando')
+// })
